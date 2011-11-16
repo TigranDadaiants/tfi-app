805543aaa94cc5727864451bfec4358e19842759
denismurashev@gmail.com <denismurashev@gmail.com@f8692dc9-1a98-fd1f-2d81-533c2e0b18f1>
2011-11-16 15:00:38 +0000

    Application UI was updated.

--- spheres/src/main/java/ru/sstu/spheres/gui/SpheresFrame.java
+++ spheres/src/main/java/ru/sstu/spheres/gui/SpheresFrame.java
@@ -18,25 +18,34 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
 import ru.sstu.images.analysis.Image;
-import ru.sstu.images.filters.BlackAndWhiteFilter;
-import ru.sstu.images.filters.edges.SobelFilter;
 import ru.sstu.properties.core.PropertyException;
 import ru.sstu.properties.gui.SettingsPanel;
 import ru.sstu.spheres.core.Sphere;
 import ru.sstu.spheres.core.SphereRecognizer;
+import ru.sstu.spheres.core.SphereRecognizerListener;
 import ru.sstu.spheres.core.SphereRecognizerSettings;
 
-public class SpheresFrame extends JFrame {
+/**
+ * <code>SpheresFrame</code> class represents main application frame.
+ *
+ * @author Denis_Murashev
+ * @since Spheres 1.0
+ */
+public class SpheresFrame extends JFrame implements SphereRecognizerListener {
+
+	private static final long serialVersionUID = 5768669984021604460L;
 
 	private static final int IMAGE_WIDTH = 500;
-	private static final int IMAGE_HEIGHT = 700;
+	private static final int IMAGE_HEIGHT = 600;
+	private static final int HISTOGRAM_WIDTH = 500;
+	private static final int HISTOGRAM_HEIGHT = 200;
 
 	private ImagePanel original = new ImagePanel(IMAGE_WIDTH, IMAGE_HEIGHT);
 	private ImagePanel processed = new ImagePanel(IMAGE_WIDTH, IMAGE_HEIGHT);
-	private HistogramPanel histogram = new HistogramPanel();
+	private HistogramPanel histogram = new HistogramPanel(HISTOGRAM_WIDTH,
+			HISTOGRAM_HEIGHT);
 	private SettingsPanel<SphereRecognizerSettings> settingsPanel;
 
 	private JFileChooser fileChooser = new JFileChooser(".");
@@ -48,15 +57,15 @@ public class SpheresFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-//		c.fill = GridBagConstraints.BOTH;
+		c.fill = GridBagConstraints.BOTH;
 		c.gridx = 0;
 		c.gridy = 0;
+		c.weighty = 1.0;
 		final int gap = 5;
 		c.insets = new Insets(gap, gap, gap, gap);
 		add(original, c);
 		c.gridx = 1;
 		add(processed, c);
-		c.weighty = 1.0;
 		c.gridx = 0;
 		c.gridy = 1;
 		add(histogram, c);
@@ -67,6 +76,26 @@ public class SpheresFrame extends JFrame {
 		setJMenuBar(createMenuBar(bundle));
 		pack();
 		setVisible(true);
+	}
+
+	@Override
+	public void imageLoaded(Image image) {
+		original.setImage(image.createImage());
+		repaint();
+	}
+
+	@Override
+	public void imageProcessed(Image image) {
+		processed.setImage(image.createImage());
+		repaint();
+	}
+
+	@Override
+	public void spheresUpdated(List<Sphere> spheres) {
+		original.setSpheres(spheres);
+		processed.setSpheres(spheres);
+		histogram.setSpheres(spheres);
+		repaint();
 	}
 
 	private JMenuBar createMenuBar(ResourceBundle bundle) {
@@ -103,44 +132,36 @@ public class SpheresFrame extends JFrame {
 
 	private void openFile() throws IOException, PropertyException {
 		if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-			Image image = new Image(ImageIO
+			final Image image = new Image(ImageIO
 					.read(fileChooser.getSelectedFile()));
-			original.setImage(image.createImage());
-			image.normalize();
-//			image.applyFilter(new MedianFilter());
-//			Filter gaussFilter = new GaussFilter(3, 1);
-//			image.applyFilter(gaussFilter);
-//			image.applyFilter(new SuperEdgeDetector());
-			image.applyFilter(new SobelFilter());
-//			image.applyFilter(new BlackAndWhiteFilter(BLACK_AND_WHITE_THREASHOLD));
-//			image.applyFilter(new MedianFilter());
-//			image.applyFilter(gaussFilter);
-			image.applyFilter(new BlackAndWhiteFilter(0.4f));
-			processed.setImage(image.createImage());
 			settingsPanel.update();
 			SphereRecognizerSettings settings = settingsPanel.getSettings();
-			SphereRecognizer recognizer = new SphereRecognizer(settings);
-			List<Sphere> spheres = recognizer.recognize(image);
-			original.setSpheres(spheres);
-			processed.setSpheres(spheres);
-			System.out.println(" Total = " + spheres.size());
-			SortedMap<Number, Integer> histogram = new TreeMap<Number, Integer>();
-			for (Sphere s : spheres) {
-				Integer r = histogram.get(s.getRadius());
-				if (r == null) {
-					r = 0;
+			final SphereRecognizer recognizer = new SphereRecognizer(settings);
+			recognizer.addListener(this);
+			Runnable runnable = new Runnable() {
+				@Override
+				public void run() {
+					List<Sphere> spheres = recognizer.recognize(image);
+					System.out.println(" Total = " + spheres.size());
+					SortedMap<Number, Integer> histogram
+							= new TreeMap<Number, Integer>();
+					for (Sphere s : spheres) {
+						Integer r = histogram.get(s.getRadius());
+						if (r == null) {
+							r = 0;
+						}
+						r++;
+						histogram.put(s.getRadius(), r);
+					}
+					for (Number n : histogram.keySet()) {
+						System.out.format("%4d : %4d%n", n.intValue(),
+								histogram.get(n));
+					}
+					repaint();
+					recognizer.removeListener(SpheresFrame.this);
 				}
-				r++;
-				histogram.put(s.getRadius(), r);
-			}
-			for (Number n : histogram.keySet()) {
-				System.out.format("%4d : %4d%n", n.intValue(), histogram.get(n));
-			}
-			repaint();
+			};
+			new Thread(runnable).start();
 		}
-	}
-
-	private static class HistogramPanel extends JPanel {
-		
 	}
 }
