909aab3882e717c7086022bef84b881022089008
sstu-tfi <dtkcommon@gmail.com>
2015-04-23 21:07:41 +0300

    Fix for Lecture Dao

--- vec/vec-core/src/main/java/ru/sstu/vec/core/dao/impl/LectureDaoImpl.java
+++ vec/vec-core/src/main/java/ru/sstu/vec/core/dao/impl/LectureDaoImpl.java
@@ -1,13 +1,29 @@
 package ru.sstu.vec.core.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import ru.sstu.vec.core.dao.LectureDao;
+import ru.sstu.vec.core.domain.Course;
 import ru.sstu.vec.core.domain.Lecture;
 
 @Repository("lectureDao")
 public class LectureDaoImpl extends GenericDao<Lecture> implements LectureDao {
 
-	private static final long serialVersionUID = 8708249726461674946L;
+    private static final long serialVersionUID = 5458043446013189190L;
+
+    private static final String COURSE = "course";
+
+    @Override
+    public List<Lecture> find(Course course) {
+        return list(getCriteria().add(Restrictions.eq(COURSE, course)));
+    }
+
+    @Override
+    public Lecture find(Course course, String name) {
+        return unique(getCriteria().add(Restrictions.eq(COURSE, course)).add(Restrictions.eq("name", name)));
+    }
 
 }
