62e9303b1f23dedb45b79eb1aee77d74a6d4569f
sstu-tfi <dtkcommon@gmail.com>
2015-06-18 22:47:15 +0300

    Hotfix:

---
+++ vec/vec-core/src/main/java/ru/sstu/vec/core/service/StudentLectureManager.java
@@ -0,0 +1,12 @@
+package ru.sstu.vec.core.service;
+
+/**
+ * {@code StudentLecturebManager} interface describes methods for lecture objects
+ * editing management.
+ *
+ * @author Tigran_Dadaiants
+ * @since VEC 2.1
+ */
+public interface StudentLectureManager extends ExpertLectureManager {
+
+}
